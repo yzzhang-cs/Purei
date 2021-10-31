2051a19f0d1194da1a9c6b4afd07ca823270c688
Ying Zhen Zhang <yzzhangcs@gmail.com>
2021-10-31 17:06:53 +0800

    Fix a bug which is caused by list id being empty

--- app/public/javascripts/player.js
+++ app/public/javascripts/player.js
@@ -1,19 +1,36 @@
 var player;
 
 function onYouTubeIframeAPIReady() {
-	player = new YT.Player('player', {
-		height: '360',
-		width: '640',
-		videoId: v,
-		playerVars: {
-			'listType': 'playlist',
-			'list': list
-		},
-		events: {
-			'onReady': onPlayerReady,
-			'onStateChange': onPlayerStateChange
-		}
-	});
+	if(list === undefined) {
+		player = new YT.Player('player', {
+			height: '360',
+			width: '640',
+			videoId: v,
+			playerVars: {
+				'playsinline': 1
+			},
+			events: {
+				'onReady': onPlayerReady,
+				'onStateChange': onPlayerStateChange
+			}
+		});
+	}
+	else {
+		player = new YT.Player('player', {
+			height: '360',
+			width: '640',
+			videoId: v,
+			playerVars: {
+				'listType': 'playlist',
+				'list': list,
+				'playsinline': 1
+			},
+			events: {
+				'onReady': onPlayerReady,
+				'onStateChange': onPlayerStateChange
+			}
+		});
+	}
 }
 
 function onPlayerReady(event) {
